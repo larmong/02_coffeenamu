33622140202c0539a0af62c159f5e1d24dfe21b9
larmong <72438781+larmong@users.noreply.github.com>
2020-10-29 11:03:48 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,39 +1,93 @@
 // main.js
 
 // 2단계 메뉴
-$(function(){
+$(function () {
     $('#gnb > .depth1 > li').hover(
-        function(){
+        function () {
             $('.depth2')
                 .stop().slideDown(400);
         },
-        function(){
+        function () {
             $('.depth2')
                 .stop().slideUp(400);
         }
     )
 })
 
-// hamburger
-$(function(){
-    $('.hamburger_icon').click(function(){
-        $('.hamburger_list').slideToggle();
-    })   
-});
-
 // banner slider
-$(function(){
+$(function () {
     var left = 0;
-    setInterval(function(){
-        if(left <= -3840) {
-            left = 0;
-        } else {
-            left = left - 1920;
-        } 
-       
-        $('.sliders')
-            // .css('maginLeft', left);
-            .animate({'marginLeft':left})
+    var img_num = 0;
+    var slider;   // slider 실행 객체 변수
+
+    $('.pager > a').removeClass('active')
+    $('.pager > a').eq(img_num).addClass('active')
+
+    function sliderStart() {
+        slider = setInterval(function () {
+            if (img_num >= 2) {
+                img_num = 0
+            } else {
+                img_num = img_num + 1;
+            }
+
+            if (left <= -3840) {
+                left = 0;
+            } else {
+                left = left - 1920;
+            }
+
+            $('.sliders')
+                .animate({ 'marginLeft': left })
             console.log(left)
-      }, 5000);
-  })
+
+            $('.pager > a').removeClass('active')
+            $('.pager > a').eq(img_num).addClass('active')
+        }, 2000);
+    }
+    function sliderStop() {
+        clearInterval(slider);
+    }
+    // slider 재생
+    sliderStart()
+    $('.slider_box, .left, .right').hover(
+        function () {
+            // 마우스 올라갔을때 슬라이더 정지
+            sliderStop();
+        },
+        function () {
+            // 마우스 내려갔을때 슬라이더 시작
+            sliderStart();
+        }
+    )
+
+    // 이전 이미지 버튼
+    $('.controls > .left').click(function () {
+        if (img_num > 0) {
+            img_num = img_num - 1
+        } else { img_num = 2; }
+        $('.pager > a').removeClass('active')
+        $('.pager > a').eq(img_num).addClass('active')
+
+
+        if (left < 0) {
+            left = left + 1920
+        } else { left = left - 3840 }
+        $('.sliders').animate({ 'marginLeft': left })
+    })
+
+    // 다음 이미지 버튼
+    $('.controls > .right').click(function () {
+        if (img_num < 2) {
+            img_num = img_num + 1
+        } else { img_num = 0; }
+        $('.pager > a').removeClass('active')
+        $('.pager > a').eq(img_num).addClass('active')
+
+
+        if (left > -3840) {
+            left = left - 1920
+        } else { left = 0 }
+        $('.sliders').animate({ 'marginLeft': left })
+    })
+})  // end$
